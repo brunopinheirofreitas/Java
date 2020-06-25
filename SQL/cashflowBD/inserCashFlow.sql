94e2ddadcdbe7e1e7ef459d6d2f68479944a32cb
unknown <bruno.pinheiro.freitas@gmail.com>
2020-06-24 21:48:42 -0300

    restarting

--- SQL/cashflowBD/inserCashFlow.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO public."Cash_Flow"(
-	nu_cash_flow, nu_expenditure_name, nu_person, dt_cash_flow, vr_cash_flow)
-	VALUES (default, 9, 1, '2019-11-05', 420.0);
--- SQL/cashflowBD/insertPerson.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO public."Person"(nu_person, no_person)VALUES (default, 'Bruno');
-INSERT INTO public."Person"(nu_person, no_person)VALUES (default, 'Talitha');
